e83baaf932e74e14818881c9aec5464c039d8a63
Gregory <gregorysjs@gmail.com>
2022-04-08 08:24:08 -0300

    1.0.0

---
+++ celsiusToFahrenheit.js
@@ -0,0 +1,8 @@
+/* celsius-to-fahrenheit.js */
+function celsiusToFahrenheit(celsius) {
+return celsius * (9/5) + 32;
+}
+const celsiusInput = process.argv[2];
+ // Pega a 3ª entrada de uma lista de argumentos 
+ const fahrenheitValue = celsiusToFahrenheit(celsiusInput);
+console.log(`${celsiusInput} degrees Celsius = ${fahrenheitValue}degrees Fahrenheit`);
